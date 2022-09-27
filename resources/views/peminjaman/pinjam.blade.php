e41a03ed34e32d05135c373c242539f796a4ec31
loraayang <loranabila41@gmail.com>
2022-09-27 12:18:47 +0700

    rubah

--- resources/views/peminjaman/pinjam.blade.php
+++ resources/views/peminjaman/pinjam.blade.php
@@ -1,21 +1,39 @@
 @extends('layouts.master')
 
 @section('content')
+
+<div class="pinjam">
 <h2>PEMINJAMAN</h2>
 
 <form action="#" method="POST">
     @csrf
-    <input type="text" name="nama_barang" placeholder="Masukan Nama Barang"><br>
-    <input type="text" name="divis" placeholder="Masukan Nama Divisi"><br>
+    <div class="input-box">
+        <label>Nama Barang</label>
+        <input type="text" name="nama_barang" placeholder="Masukan Nama Barang">
+    </div>
+    <div class="input-box">
+        <label>Divisi</label>
+        <input type="text" name="divisi" placeholder="Masukan Nama Divisi">
+    </div>
+    <div class="select-box">
+    <label>Katagori</label>
     <select name="katagori">
-        <option value="">Pilih Katagori Barang</option>
-        <option value="laptop">Laptop</option>
-        <option value="led_tv">LED TV</option>
-        <option value="infocus">Infocus</option>
-        <option value="printer">Printer</option>
-    </select><br>
-    <input type="text" name="quantity" placeholder="Masukan Quantity"><br>
-    <input type="submit" name="submit" value="kirim">
+            <option value="">Pilih Katagori Barang</option>
+            <option value="laptop">Laptop</option>
+            <option value="led_tv">LED TV</option>
+            <option value="infocus">Infocus</option>
+            <option value="printer">Printer</option>
+        </select>
+    </div>
+    <div class="input-box">
+        <label>Quantity</label>
+        <input type="text" name="quantity" placeholder="Masukan Quantity">
+    </div>
+    <div class="sumbit">
+        <input type="submit" name="submit" value="kirim">
+    </div>
 </form>
+</div>
+
 
 @endsection
